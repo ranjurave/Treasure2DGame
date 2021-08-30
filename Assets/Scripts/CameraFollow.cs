7d439a883952f9c24a9023b1616a59cf81124a55
Ranju <ranjurave@gmail.com>
2021-08-30 18:15:30 +1200

    WIP
    Jump
    RuleTile
    Camera movements

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 cameraOffset;
+
+    void Update()
+    {
+        Vector3 newPosition = player.transform.position + cameraOffset ;
+        Vector3 smoothedCamera = Vector3.Lerp(transform.position, newPosition, 10 * Time.deltaTime);
+        transform.position = smoothedCamera;
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerMovement : MonoBehaviour {
     Rigidbody2D playerRB;
     public float moveSpeed;
     Animator playerAnimator;
+    BoxCollider2D jumpCollider;
 
     void Start() {
         playerRB = GetComponent<Rigidbody2D>();
         playerAnimator = GetComponent<Animator>();
+        jumpCollider = GetComponent<BoxCollider2D>();
+    }
+    void Update() {
+        Move();
+        Jump();
     }
 
-    void Update() {
+    private void Move() {
         //Player movements
         float movement = Input.GetAxis("Horizontal");
         Vector2 playerVelocity = new Vector2(movement * moveSpeed, playerRB.velocity.y);
@@ -26,5 +29,17 @@ public class PlayerMovement : MonoBehaviour {
 
         //Change Boolean parameter in Animator Controller to play walk animation
         playerAnimator.SetBool("CanWalk", playerHasHoizontalMovment);
+    }
+
+    private void Jump() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (jumpCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) {
+                playerAnimator.SetBool("CanJump", true);
+                playerRB.velocity += new Vector2(0, 5);
+            }
+        }
+        else {
+            playerAnimator.SetBool("CanJump", false);
+        }
     }
 }
