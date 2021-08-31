0f83e231fe5e283770914d1c3f9f2577e3f0a4b6
Ranju <ranjurave@gmail.com>
2021-08-31 16:45:58 +1200

    WIP Day4
    Climbing added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,19 @@ public class PlayerMovement : MonoBehaviour {
     public float moveSpeed;
     Animator playerAnimator;
     BoxCollider2D jumpCollider;
+    CapsuleCollider2D playerCollider;
+    public float climbSpeed = 2.0f;
 
     void Start() {
         playerRB = GetComponent<Rigidbody2D>();
         playerAnimator = GetComponent<Animator>();
         jumpCollider = GetComponent<BoxCollider2D>();
+        playerCollider = GetComponent<CapsuleCollider2D>();
     }
     void Update() {
         Move();
         Jump();
+        Climb();
     }
 
     private void Move() {
@@ -40,6 +44,25 @@ public class PlayerMovement : MonoBehaviour {
         }
         else {
             playerAnimator.SetBool("CanJump", false);
+        }
+    }
+
+    private void Climb() {
+        // if player is colliding with ladder layer
+        if (playerCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))) {  
+            if (Input.GetKey(KeyCode.UpArrow)) {
+                Vector2 climbVelocity = new Vector2(playerRB.velocity.x, climbSpeed);
+                playerRB.velocity = climbVelocity;
+                playerAnimator.SetBool("CanClimb", true);
+            }
+            else {
+                playerRB.gravityScale = 0; // setting gravity to 0 so that the player will not drift down
+                playerRB.velocity = new Vector2(playerRB.velocity.x, 0); // stopping the upward push
+            }
+        }
+        else {
+            playerRB.gravityScale = 1; // restoring gravity
+            playerAnimator.SetBool("CanClimb", false);
         }
     }
 }
